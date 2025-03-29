4eb01fae7f29ee2833ec7e2d39e3a03aab476ef3
Anonymousxxx69 <spiderman19374638@gmail.com>
2025-03-29 11:39:08 +0530

    Update utils.py

--- helper/utils.py
+++ helper/utils.py
@@ -118,7 +118,8 @@ def makedir(name: str):
 
 
 
-# Jishu Developer 
+# JOIN @thealphabotz FOR MORE FANATIC REPOSITORIES
 # Don't Remove Credit 🥺
-# Telegram Channel @JishuBotz
-# Developer @JishuDeveloper
+# Telegram Channel @thealphabotz
+# Developer @Trashxrd
+# SPECIAL THANKS TO @UNKNOWN_RK01
